c208d155adc7d888ddd49c2a410a5199fab3cc6e
Ashton Omdahl <aomdahl@gmail.com>
2022-05-21 13:46:25 -0400

    major typoe

--- udler_td2/src/run_ldsc.all.sh
+++ udler_td2/src/run_ldsc.all.sh
@@ -16,3 +16,12 @@ for i in {0..9}; do
 cat /work-zfs/abattle4/ashton/snp_networks/scratch/udler_td2/ldsr_all/run_scripts/slurm_template.sh /work-zfs/abattle4/ashton/snp_networks/scratch/udler_td2/ldsr_all/ldsr_results/_ldsc.run.${i}.sh > run_scripts/slurm.ldsc.run.${i}.sh
 sbatch run_scripts/slurm.ldsc.run.${i}.sh
 done
+
+#once the jobs are completed, extract the relevant information.
+P=/work-zfs/abattle4/ashton/snp_networks/scratch/udler_td2/src
+for i in ldsr_results/*.log; do
+    echo $i
+    python $P/parseLDSCr2g.py --input_file $i --output ldsr_results/
+    b=`fileName $i`
+    grep -A 1000 "gcov_int_se" $i  | grep -v "Analysis finished" | grep -v "Total time" > ldsr_results/$b.rg_report.tsv
+done
