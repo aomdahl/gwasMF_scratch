490b9f271c03ec31f6091c90390c87fda9586200
aomdahl1 <aomdahl1@login01.rockfish.cluster>
2022-06-08 16:00:59 -0400

    updating paths to rockfish

--- gwasMF_pleiotropy/pleiotropyQuickAssess.R
+++ gwasMF_pleiotropy/pleiotropyQuickAssess.R
@@ -19,6 +19,7 @@ parser$add_argument("--alpha", type = 'numeric', help = "alpha threshold",
 parser$add_argument("--maf", type = 'character', help = "specify where the MAFs can be found. An ordered column will do.", 
                     default = "")
 parser$add_argument("--output", type = 'character', help = "specify path to write ouput files", default = "")
+parser$add_argument("--tab_output", type = 'logical', help = "Specify this flag to write out tabular files", action = "store_true", default = FALSE)
 parser$add_argument("--debug_mode", action = "store_true", default = FALSE, help = "Specify this to run in deubg mode.", type = "logical")
 parser$add_argument('--help',type='logical',action='store_true',help='Print the help page')
 parser$helpme()
@@ -44,12 +45,17 @@ if(args$Z == "SEED2" | debug)
 
 if(debug)
 {
-  #l.path <- "/work-zfs/abattle4/ashton/snp_networks/custom_l1_factorization/results/seed2_pleio/factorization/A2.14_L961.269_B_SE.loadings.txt"
-  #f.path <- "/work-zfs/abattle4/ashton/snp_networks/custom_l1_factorization/results/seed2_pleio/factorization/A2.14_L961.269_B_SE.factors.txt"
+  l.path <- "/work-zfs/abattle4/ashton/snp_networks/custom_l1_factorization/results/seed2_pleio/factorization/A2.14_L961.269_B_SE.loadings.txt"
+  f.path <- "/work-zfs/abattle4/ashton/snp_networks/custom_l1_factorization/results/seed2_pleio/factorization/A2.14_L961.269_B_SE.factors.txt"
+  args$output <- "/work-zfs/abattle4/ashton/snp_networks/custom_l1_factorization/results/seed2_pleio/factorization/A2.14_L961.269_B_SE"
   
-  l.path <- "/work-zfs/abattle4/ashton/snp_networks/custom_l1_factorization/results/seed2_pleio_L/factorization/A2.14_L4806.343_B_SE.loadings.txt"
-  f.path <- "/work-zfs/abattle4/ashton/snp_networks/custom_l1_factorization/results/seed2_pleio_L/factorization/A2.14_L4806.343_B_SE.factors.txt"
-  args$output <- "/work-zfs/abattle4/ashton/snp_networks/custom_l1_factorization/results/seed2_pleio_L/factorization/A2.14_L4806"
+  #l.path <- "/work-zfs/abattle4/ashton/snp_networks/custom_l1_factorization/results/seed2_pleio_L/factorization/A2.14_L4806.343_B_SE.loadings.txt"
+  #f.path <- "/work-zfs/abattle4/ashton/snp_networks/custom_l1_factorization/results/seed2_pleio_L/factorization/A2.14_L4806.343_B_SE.factors.txt"
+  
+  l.path <- "/work-zfs/abattle4/ashton/snp_networks/custom_l1_factorization/results/seed2_gold_standard/factorization/"
+  f.path <- "/work-zfs/abattle4/ashton/snp_networks/custom_l1_factorization/results/seed2_pleio/factorization/A2.14_L961.269_B_SE.factors.txt"
+  args$output <- "/work-zfs/abattle4/ashton/snp_networks/custom_l1_factorization/results/seed2_pleio/factorization/A2.14_L961.269_B_SE"
+  
 } else {
   l.path <- args$L_mat
   f.path <- args$F_mat
@@ -76,10 +82,15 @@ alpha = args$alpha
 #alpha = 0.01
 #FA us fast asset, nOT FACTOR ANALYSIS.
 #Default setting- filter by only significant SNPs (?)
+if(ncol(fdr.pvals) != K)
+{
+    message("Error in projecting FDRs...")
+}
 joined.pleio <- left_join(fin, fdr.pvals, by = "snpid")  %>% 
   set_colnames(c("cpos", "cneg", "pval", "SNP_num", "snp_id","fa_fdr", paste0("FDR_", 1:K))) %>%
   filter(fa_fdr < alpha)
 
+message(paste0("Latent space has ", K, " factors"))
 #different question- are significant L1's enriched for meta-analysis significance?
 
 #Put all of them onto 1 plot?
@@ -118,7 +129,7 @@ for(i in 1:K)
 
 #All conjoined on one plt...
 factor.enrichment.joined$Factor <- factor(factor.enrichment.joined$Factor, levels = paste0("F", 1:K)[order(t.stats,decreasing = TRUE)])
-ggplot(factor.enrichment.joined, aes(x = Factor, y = pleio_score, fill = latent_sig), position = position_dodge()) + labs(fill = "FDR") + 
+ggplot(factor.enrichment.joined, aes(x = Factor, y = pleio_score, fill = latent_sig), position = position_dodge()) + labs(fill = "Latent factor\nFDR") + 
 scale_fill_manual(labels = c(paste0("> ", alpha), paste0("< ", alpha)), values = c("coral", "skyblue"))+
   geom_boxplot() + theme_minimal(16) + ylab("Pleiotropy score") + ggtitle("Pleiotropy of significant SNPs by latent\nfactor (ordered by t-stat)")
 ggsave(paste0(args$output, ".combined_pleiotropy.png"),width = 10)
@@ -126,9 +137,9 @@ ggsave(paste0(args$output, ".combined_pleiotropy.png"),width = 10)
 #####
 message("Including PVE, N")
 #Another option would be to order tehre by the PVE they each have. 
-pma.style <- pveBySVD(as.matrix(f.matrix[,-1]),as.matrix(l.matrix[,-1]), as.matrix(Z), K = 15)
+pma.style <- pveBySVD(as.matrix(f.matrix[,-1]),as.matrix(l.matrix[,-1]), as.matrix(Z), K = K)
 flashr.style <- approxPVE_init(as.matrix(Z), as.matrix(f.matrix[,-1]),as.matrix(l.matrix[,-1]))
-flashr.style2 <- approxPVE(as.matrix(f.matrix[,-1]),as.matrix(l.matrix[,-1]), as.matrix(Z), K = 15)
+flashr.style2 <- approxPVE(as.matrix(f.matrix[,-1]),as.matrix(l.matrix[,-1]), as.matrix(Z), K = K)
 order(pma.style, decreasing = TRUE)
 order(flashr.style, decreasing = TRUE)
 order(flashr.style2, decreasing = TRUE)
@@ -156,7 +167,7 @@ ggsave(paste0(args$output, ".pve_sample_pleio.png"))
 message("Checking for linear enrichment now...")
 cont.test <- data.frame(l.matrix) %>% rename("snpid" = snps) %>% mutate("maf" = maf) %>%
   merge(., fin, by = "snpid") %>% filter(fa_fdr < alpha) %>% mutate("pleio_score" = cpos + cneg)
-joint.pleio.enrichment <- lm(cont.test$pleio_score ~ abs(as.matrix(cont.test %>% select(paste0("X", 1:15), maf))))
+joint.pleio.enrichment <- lm(cont.test$pleio_score ~ abs(as.matrix(cont.test %>% select(paste0("X", 1:K), maf))))
 joint.dat <- data.frame(summary(joint.pleio.enrichment)$coefficients) %>% 
   set_colnames(c("Beta", "SE", "T", "P")) %>% mutate("Factor" = c("Intercept", paste0("F", 1:K), "MAF"))
 
@@ -175,30 +186,19 @@ ggsave(paste0(args$output, ".indep_pleiotropy.png"))
 
 ###########
 #Look at top loaded SNPS in l matrix- are these
-if(FALSE)
-{
-  #Do it by FDR
-  scores <- NULL
-  for(a in seq(0,15, by = 1))
-  {
-    t <- factor.enrichment.joined %>% filter(Factor == "F1") %>% 
-      filter(-log10(latent_fdr) > a) %>% select(pleio_score) %>% mutate("FDR_thresh" = a)
-    scores <- rbind(scores, t)
-  }
-  ggplot(scores, aes(x = (FDR_thresh), y = pleio_score, group = as.factor(FDR_thresh))) + geom_boxplot()
-}
+
 #Do it by L matrix
 #cumulative
 scores <- NULL
 t <- cont.test %>% select(pleio_score, X1) 
 t$squared.cos.score <- (t$X1)^2/sum((t$X1)^2)
-for(a in seq(0,15, by = 0.25))
+for(a in seq(0,K, by = 0.25))
 {
   f <- t %>% filter(-log10(squared.cos.score) < a) %>% select(pleio_score) %>% mutate("Score_thresh" = a)
   scores <- rbind(scores, f)
 }
-ggplot(scores, aes(x = Score_thresh, y = pleio_score, group = as.factor(Score_thresh))) + geom_boxplot() + theme_minimal()
-
+ggplot(scores, aes(x = Score_thresh, y = pleio_score, group = as.factor(Score_thresh))) + geom_boxplot() + theme_minimal() + xlab("SNP loading score threshold") + ylab("Pleiotropy score") + ggtitle("Pleiotropy score by SNP loading in Factor 1")
+ggsave(file = paste0(args$output, ".cumulative_bins_pleio_score.png"))
 #totally stratified....
 for(i in 1:K)
 {
@@ -212,11 +212,11 @@ for(i in 1:K)
     scores.strat <- rbind(scores.strat, f)
     prev = a
   }
-  print(ggplot(scores.strat, aes(x = Score_thresh, y = pleio_score, group = as.factor(Score_thresh))) + 
+  ggplot(scores.strat, aes(x = Score_thresh, y = pleio_score, group = as.factor(Score_thresh))) + 
     geom_boxplot(varwidth = TRUE)+ theme_minimal() +
     geom_smooth(method = "loess", se=FALSE, color="blue", aes(group=1), linetype = "12") + xlab("Score Bin")+
-    ylab("Pleiotropy score") + ggtitle(paste0("Plieotropy score across factor", i,  " score bins")))
-  
+    ylab("Pleiotropy score") + ggtitle(paste0("Plieotropy score across factor", i,  " score bins"))
+    ggsave(file = paste0(args$output, ".stratified_bins_factor", i, ".png")) 
 }
 
 
@@ -225,17 +225,65 @@ for(i in 1:K)
 
 scaled.l <- apply(l.matrix[,-1], 2, function(x) x/norm(x,type = "2"))
 cosine.scores.l <- data.frame(t(apply(scaled.l, 1, function(x) x^2/sum(x^2)))) %>% mutate("snpid" = l.matrix$snps)
+
+
 top.pleio.snps <- fin  %>% mutate("pleio_score" = cpos + cneg) %>% select(pleio_score, snpid, fa_fdr) #%>% filter(fa_fdr < alpha)
 cosine.scores.pleio.scores <- left_join(top.pleio.snps, cosine.scores.l, by = "snpid")
-cosine.scores.pleio.scores$top_factor <- apply(cosine.scores.pleio.scores[,3:17],1, function(x) which.max(x))
-cosine.scores.pleio.scores$relative_max_contribution <- apply(cosine.scores.pleio.scores[,3:17],1, function(x) max(x))
+cosine.scores.pleio.scores$top_factor <- apply(cosine.scores.pleio.scores[,3:(K+2)],1, function(x) which.max(x))
+cosine.scores.pleio.scores$relative_max_contribution <- apply(cosine.scores.pleio.scores[,3:(K+2)],1, function(x) max(x))
 cosine.scores.pleio.scores <- cosine.scores.pleio.scores %>% mutate("fastAsset_sig" = ifelse(fa_fdr < alpha, "sig", "nonsig"))
-ggplot(cosine.scores.pleio.scores, aes(y = pleio_score, x = top_factor, color = -log10(fa_fdr) )) + geom_jitter()
+if(args$tab_output)
+{
+  #write the cosine scores 
+  write.table(cosine.scores.pleio.scores %>% select(snpid,pleio_score,fa_fdr,fastAsset_sig,top_factor,relative_max_contribution, paste0("X", 1:K)), file = paste0(args$output, ".sig_vs_nonsig.tsv"),quote = FALSE,row.names = FALSE,sep = '\t' )
+}
 
-ggplot(cosine.scores.pleio.scores, aes(y = pleio_score, x = as.factor(top_factor), color = fastAsset_sig )) + 
+
+df.cspc <- cosine.scores.pleio.scores %>% select(pleio_score, snpid, fa_fdr, top_factor, fastAsset_sig,relative_max_contribution)
+#df.cspc <- df.cspc %>% mutate("pres_score" = ifelse(fastAsset_sig == "sig", -pleio_score, pleio_score))
+ggplot(df.cspc, aes(y = pleio_score, x = as.factor(top_factor), color = fastAsset_sig )) + 
   geom_jitter() + xlab("Top scoring Latent factor") + ylab("Pleiotropy score") + 
   labs(color = "fastAsset FDR < 0.01") + ggtitle("Top factor assignment of each SNP") + theme_minimal()
 
-ggplot(cosine.scores.pleio.scores %>% filter(fastAsset_sig == "sig"), aes(y = pleio_score, x = as.factor(top_factor), color= relative_max_contribution)) + 
-  geom_jitter() + xlab("Top scoring Latent factor") + ylab("Pleiotropy score") + 
-  labs(color = "Relative contribution of top factor") + ggtitle("Top factor assignment of each SNP") + theme_minimal()
+#It looks like the easiset thing will be just to hack a plain old miami plot...
+tmp <- df.cspc %>% group_by(top_factor) %>% filter(row_number()==1) %>% 
+  mutate(pleio_score = NA, fastAsset_sig = "sig", "opacity" = 0.00, "color_key" = NA) %>% filter(top_factor == 1)
+df.miami <- df.cspc %>% mutate("opacity" = 1, "color_key" = ifelse((top_factor %% 2) ==0, "skyblue", "blue")) %>% bind_rows(., tmp)
+#library(miamiplot)
+#ggmiami(data = data.frame(df.miami), split_by = "fa_fdr", split_at = 0.01, 
+        #p = "pleio_score", chr_colors = NULL, upper_chr_colors = c("skyblue", "blue"), lower_chr_colors =c("coral", "red"))
+
+#no, sadly no.
+#upper
+upper <- ggplot(df.miami %>% filter(fastAsset_sig == "sig"), aes(y = pleio_score, x = as.factor(top_factor), alpha = opacity,color = color_key)) + 
+  geom_jitter() + xlab("Top scoring Latent factor") + ylab(paste("FDR < ", alpha)) + 
+  theme_classic(15) + scale_colour_manual(values =c("skyblue", "blue")) + 
+  theme(legend.position = "none", axis.title.x = ggplot2::element_blank(), 
+  plot.margin = ggplot2::margin(t = 10, b = 0, l = 10, r = 10))
+
+lower <- ggplot(df.miami %>% filter(fastAsset_sig != "sig"), aes(y = pleio_score, x = as.factor(top_factor),color = color_key)) + 
+  geom_jitter() + xlab("Top scoring Latent factor") + 
+  labs(color = "fastAsset FDR < 0.01", x="") +  theme_classic(15) + scale_y_reverse() + 
+  scale_colour_manual(values =c("red", "coral")) +  
+  ggplot2::scale_x_discrete(position = "top") + theme(legend.position = "none", axis.title.x = ggplot2::element_blank(), 
+                                                      axis.text.x=element_blank(),
+                                                        plot.margin = ggplot2::margin(t = 0, b = 0, l = 10, r = 10)) + 
+  ylab(paste("FDR > ", alpha))
+
+g <- gridExtra::grid.arrange(upper, lower, nrow = 2, top = "Pleiotropy scores for top loaded factor by SNP")
+
+ggsave(g, file = paste0(args$output, ".sig_vs_nonsig.png"))
+
+#df.cspc <- rbind(df.cspc %>% filter(fastAsset_sig == "sig"), data.frame(c(0, "na", NA, 1, "sig", NA)))
+
+ggplot(df.cspc %>% filter(fastAsset_sig == "sig"), aes(y = pleio_score, x = as.factor(top_factor), color= relative_max_contribution)) + 
+  geom_jitter() + xlab("Latent factor") + ylab("Pleiotropy score") + scale_colour_gradient2(low = "grey", high = "red") + 
+  labs(color = "Relative contribution of\nfactor") + ggtitle("Relative factor weight for fastAsset FDR < 0.01 SNPs") + theme_minimal()
+ggsave(paste0(args$output, ".factor_weights_FA_sig_snps.png"))
+
+ggplot(df.cspc %>% filter(fastAsset_sig != "sig"), aes(y = pleio_score, x = as.factor(top_factor), color= relative_max_contribution)) + 
+  geom_jitter() + xlab("Top scoring Latent factor") + ylab("Pleiotropy score") + scale_colour_gradient2(low = "grey", high = "red") +
+  labs(color = "Relative contribution of\nfactor") + ggtitle("Relative factor weight for fastAsset FDR > 0.01 SNPs") + theme_minimal(15)
+ggsave(paste0(args$output, ".factor_weight_FA_NONsig_snps.png"))
+
+df.cspc %>% filter(fastAsset_sig != "sig") %>% group_by(top_factor) %>% summarize("n" = n())
