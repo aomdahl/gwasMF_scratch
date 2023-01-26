9cee5f64ceb38ba5ab72a11c564366c3594f3723
aomdahl <aomdahl1@gmail.com>
2023-01-26 09:55:53 -0500

    adding in bic variance esetimate control

--- cohort_overlap_exploration/src/runSimulation.sh
+++ cohort_overlap_exploration/src/runSimulation.sh
@@ -7,21 +7,32 @@ ml anaconda
     YAML=$1
     #YAML=simulating_factors/udler_based_500/udler_1_3block-corr_realistic-high-overlap/udler_1_3block-corr_realistic-high-overlap.yml 
     ODIR=$2
+    mkdir -p $ODIR
+    b=`basename $YAML`
+    echo "Currently running $b"
     #ODIR=simulating_factors/udler_based_500/udler_1_3block-corr_realistic-high-overlap/
+    #First: create all the simulations. with niter, each at a different seed
     NITER=`grep "iter" $YAML | cut -f 2 -d ","` #given in the yaml file
     for i in $(eval echo "{1..$NITER}"); do
-        Rscript src/generateGWASFactors.R --input $YAML -o ${ODIR}/sim${i} --seed ${i}
+        echo "iter $i"
+	 Rscript src/generateGWASFactors.R --input $YAML -o ${ODIR}/sim${i} --seed ${i}
     done
 
     #Now actually run the factorization.
     mkdir -p ${ODIR}/factorization_results
     MNAMES=`grep "test_methods" $YAML | cut -f 2 -d "," | sed 's/:/,/g'`
-    K=`grep "K" $YAML | cut -f 2 -d ","`
+    K=`grep -e "^K," $YAML | cut -f 2 -d ","`
+    BIC=`grep "bic_param," $YAML | cut -f 2 -d ","`
     for i in $(eval echo "{1..$NITER}"); do
-        Rscript /home/aomdahl1/scratch16-abattle4/ashton/snp_networks/gwas_decomp_ldsc/src/matrix_factorization.R --se_data ${ODIR}/sim${i}.std_error.txt --beta_data ${ODIR}/sim${i}.effect_sizes.txt --seed ${i} --outdir ${ODIR}/factorization_results/sim${i}. --only_run $MNAMES --K $K --no_plots 
+	echo "Simulation iter $i"
+        Rscript /home/aomdahl1/scratch16-abattle4/ashton/snp_networks/gwas_decomp_ldsc/src/matrix_factorization.R  \
+        --se_data ${ODIR}/sim${i}.std_error.txt --beta_data ${ODIR}/sim${i}.effect_sizes.txt --seed ${i} \
+        --outdir ${ODIR}/factorization_results/sim${i}. --only_run $MNAMES --K $K --no_plots --bic_var $BIC 
 done
 
 #assess the performance of this...
 Rscript src/evaluateSims.R --output ${ODIR}/factorization_results/summary --plot --yaml  $YAML --sim_path ${ODIR}/factorization_results/ 
 
-
+echo "END OF FULL SIMULATION"
+echo " "
+echo  " "
